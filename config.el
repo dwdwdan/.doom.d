6627bba6478fd6368a6cf82b06a828a677ae20cf
dwdwdan <dan.walters5@outlook.com>
2021-09-14 09:34:52 +0100

    Remove excess comments

--- config.el
+++ config.el
@@ -32,14 +32,8 @@
 (setq doom-font (font-spec :family "Jetbrains Mono" :size 20)
       doom-variable-pitch-font (font-spec :family "Cantarell" :size 20))
 
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dracula)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory (concat dropbox-dir "/Org")
       org-roam-directory (concat dropbox-dir "/OrgRoam")
       org-ellipsis " ▼")
@@ -57,8 +51,6 @@
 
 (setq +latex-viewers `(pdf-tools))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type `relative)
 
 
